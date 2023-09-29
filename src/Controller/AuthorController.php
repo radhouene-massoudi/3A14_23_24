9e4c80881b21c4c1a24021cf0ee4c0df04f86ba5
radhouene-massoudi <massoudiradhouene@gmail.com>
2023-09-29 16:32:28 +0100

    seance3

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -8,6 +8,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AuthorController extends AbstractController
 {
+    public $authors = array(
+        array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' =>
+        'victor.hugo@gmail.com ', 'nb_books' => 100),
+        array('id' => 2, 'picture' => '/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>
+        ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
+        array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>
+        'taha.hussein@gmail.com', 'nb_books' => 300),
+        );
     #[Route('/author', name: 'app_author')]
     public function index(): Response
     {
@@ -17,16 +25,17 @@ class AuthorController extends AbstractController
     }
     #[Route('/show', name: 'show')]
     public function showAuthor(){
-        $authors = array(
-            array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' =>
-            'victor.hugo@gmail.com ', 'nb_books' => 100),
-            array('id' => 2, 'picture' => '/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>
-            ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
-            array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>
-            'taha.hussein@gmail.com', 'nb_books' => 300),
-            );
+        
            return $this->render('author/list.html.twig',[
-            'tab'=>$authors
+            'tab'=>$this->authors
            ]);
+    }
+    #[Route('/details/{id}', name: 'details')]
+    public function authorDetails($id){
+        return $this->render('author/details.html.twig',
+        [
+            'id'=>$id,
+            'tab'=>$this->authors
+        ]);
     }
 }
